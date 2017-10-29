2118e0c9fde87c0352df6e9bfed827b933de31c2
Anjali Krishnan <AKrishnan@esri.com>
2017-10-29 18:45:04 +0400

    added playermodel

---
+++ MafiaServer/MafiaServerWPF/Models/PlayerModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MafiaServerWPF.Models
+{
+	public class PlayerInfo
+	{
+		public PlayerInfo()
+		{
+			Name = "";
+			IP = "localhost";
+			Port = 8000;
+		}
+		public string Name { get; set; }
+		public string IP { get; set; }
+		public int Port { get; set; }
+
+	}
+
+	public class PlayManagerModel
+	{
+		public List<PlayerInfo> _playerList;
+		
+		ServerModel _server;
+		PlayManagerModel(ServerModel server)
+		{
+			_server = server;
+			_server.PlayerJoined += _server_PlayerJoined;
+		}
+
+		private void _server_PlayerJoined(object sender, EventArgs args)
+		{
+			
+
+		}
+
+		
+	}
+}
--- MafiaServer/MafiaServerWPF/ViewModels/PlayManagerViewModel.cs
+++ MafiaServer/MafiaServerWPF/ViewModels/PlayManagerViewModel.cs
@@ -3,29 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CommonUtils;
+using MafiaServerWPF.Models;
 
 namespace MafiaServerWPF.ViewModels
 {
-    public struct PlayerInfo
-    {
-        public string Name { get; set; }
-    }
-    public class PlayManagerViewModel: BaseViewModel
-    {
-        private string _test;
-        private bool _isVisible = true;
+	
 
-        public bool isPlayManagerVisible
-        {
-            get { return _isVisible; }
-            set { SetField(ref _isVisible, value, "isPlayManagerVisible"); }
-        }
+	public class PlayManagerViewModel : BaseViewModel
+	{
+		private string _test;
+		private bool _isVisible = true;
+		private PlayManagerModel _playManagerModel = new PlayManagerModel();
 
-        public string Test
-        {
-            get { return _test; }
-            set { SetField(ref _test, value, "Test"); }
-        }
+		public PlayManagerViewModel()
+		{
+			PlayerList = new List<PlayerInfo>();
+			PlayerList.Add(new PlayerInfo() { Name = "Anjali" });
+			PlayerList.Add(new PlayerInfo() { Name = "Sachin" });
+			PlayerList.Add(new PlayerInfo() { Name = "Meera" });
+		}
 
-    }
+		public List<PlayerInfo> PlayerList
+		{
+			get { return _playManagerModel._playerList; }
+			set { SetField(ref _playManagerModel._playerList, value, "PlayerList"); }
+		}
+
+		public bool isPlayManagerVisible
+		{
+			get { return _isVisible; }
+			set { SetField(ref _isVisible, value, "isPlayManagerVisible"); }
+		}
+
+		public string Test
+		{
+			get { return _test; }
+			set { SetField(ref _test, value, "Test"); }
+		}
+
+	}
 }
