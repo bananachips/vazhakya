f33d58759f3a4e7e7518b97b90947ffa594533d5
Banana Chips <vazhakya@gmail.com>
2017-11-02 16:57:15 +0400

    test for shuffle

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,26 +11,42 @@ namespace Test
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void TestRandomUtil()
+		{
+			List<string> names = new List<string>() { "Anjali", "Anna", "Bindiya", "Chayanika", "Diana", "Ellen", "Farah", "Gina" };
+			CommonUtils.Utilities.Shuffle(ref names);
+			names.ForEach(Console.WriteLine);
+			
+		}
+
+		void SerializationTest()
 		{
 			KeyValue<int> x = new KeyValue<int> { key = "age", value = 100 };
 			string st = x.GetXml();
 			Type tt = x.GetType();
-			KeyValue<int> xy= new KeyValue<int>(st);
+			KeyValue<int> xy = new KeyValue<int>(st);
 			KeyValue<string> d = new KeyValue<string> { key = "age", value = "100" };
 			tt = x.GetType();
 
 			XmlSerializer xmlSerializer = new XmlSerializer(x.GetType());
 			StringWriter textWriter = new StringWriter();
 
-			
+
 			xmlSerializer.Serialize(textWriter, x);
 			string str = textWriter.ToString();
-		
+
 			StringReader textReader = new StringReader(str);
 			XmlSerializer xml = new XmlSerializer(x.GetType());
 			KeyValue<int> t = (KeyValue<int>)xmlSerializer.Deserialize(textReader);
-			
+		}
+		static void Main(string[] args)
+		{
+			do
+			{
+				TestRandomUtil();
+				Console.WriteLine("Press q to stop");
+			}
+			while (Console.Read() != 'q');
 		}
 	}
 }
